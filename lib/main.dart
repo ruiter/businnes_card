1821b33c5e5e683b7da43fe3ad8b6760c7937447
Ruiter Matos <ruitercomp@gmail.com>
2020-04-08 09:37:04 -0300

    Add Column and new styles for UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,27 +12,60 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
-                backgroundImage: NetworkImage('https://pbs.twimg.com/profile_images/1053055123193122816/IUwo6l_Q_400x400.jpg'),
+                backgroundImage: NetworkImage(
+                    'https://pbs.twimg.com/profile_images/1053055123193122816/IUwo6l_Q_400x400.jpg'),
               ),
               Text(
                 'Kakaroto',
                 style: TextStyle(
                     fontFamily: 'Pacifico',
                     fontSize: 30.0,
-                    color: Colors.white
-                ),
+                    color: Colors.white),
               ),
               Text(
                 'Salvador da Terra',
                 style: TextStyle(
                     fontFamily: 'Source Sans Pro',
                     fontSize: 20.0,
-                    color: Colors.white
+                    color: Colors.white),
+              ),
+              SizedBox(
+                height: 10.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
                 ),
-              )
+              ),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 20.0, horizontal: 25.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.phone,
+                        color: Colors.teal,
+                      ),
+                      title: Text('+55 64 99899-7865',
+                          style: TextStyle(
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20.0,
+                              color: Colors.teal.shade900)))),
+              Card(
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.teal,
+                      ),
+                      title: Text('kakaroto@gmail.com',
+                          style: TextStyle(
+                              fontFamily: 'Source Sans Pro',
+                              fontSize: 20.0,
+                              color: Colors.teal.shade900))))
             ],
           ),
         ),
